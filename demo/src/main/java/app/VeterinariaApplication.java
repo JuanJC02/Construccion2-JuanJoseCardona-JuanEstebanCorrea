0ee23186dc8869cea9ecb8119b0f6cc5abe2a68d
Usuario <Usuario@DESKTOP-4DM4G3V>
2025-09-30 22:55:05 -0500

    .

--- demo/src/main/java/app/VeterinariaApplication.java
+++ demo/src/main/java/app/VeterinariaApplication.java
@@ -2,12 +2,14 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import app.domain.adapters.input.ConsoleAdapter;
 
 @SpringBootApplication
 public class VeterinariaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VeterinariaApplication.class, args);
+		ConsoleAdapter.showprincipalmenu();
 	}
 
 }
